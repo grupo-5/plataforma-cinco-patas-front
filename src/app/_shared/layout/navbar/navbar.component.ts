8c0687b00f52a1bcccae5f98994dd7a626a52efc
Rosana Damasceno <rosanaxica7@yahoo.com.br>
2020-09-17 22:38:31 -0300

    navbar pronta aguardando botao

--- src/app/_shared/layout/navbar/navbar.component.ts
+++ src/app/_shared/layout/navbar/navbar.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, NgModule } from '@angular/core';
+
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.css'],
 })
 export class NavbarComponent implements OnInit {
+  // @Input() options: {}
 
-  constructor() { }
+  options = [
+    { name: 'home', route: '/admin', type: 'link' },
+    { name: 'Sobre Nos', route: '/admin', type: 'link' },
+    { name: 'Quero Ajudar', route: '/admin', type: 'link' },
+    { name: 'Depoimentos', route: '/admin', type: 'link' },
+    { name: 'Ongs Parceiras', route: '/admin', type: 'link' },
+    { name: 'Login', route: '/admin', type: 'button' },
 
-  ngOnInit(): void {
-  }
+  ];
 
+  constructor() {}
+
+  ngOnInit(): void {}
 }
