e85d7dcb6b45e0773f5f190f0a7ef6f0979d78dd
Tathi <tathianesouza1@gmail.com>
2020-09-24 16:41:20 -0300

    Altera os componentes para torná-los reativos

--- src/app/_shared/components/card-grafico/card-grafico.component.ts
+++ src/app/_shared/components/card-grafico/card-grafico.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { normalizePassiveListenerOptions } from '@angular/cdk/platform';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-card-grafico',
@@ -7,9 +8,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CardGraficoComponent implements OnInit {
 
+  myDoughnutData: Array<any>;
+  myDoughnutLabels: Array<any>;
+  @Input() titulo: string;
+  myListElements: Array<any>;
+  myPercentage: Array<any>;
+  novo: Array<any>;
+  total: number;
+
   constructor() { }
 
   ngOnInit(): void {
+
+    this.myDoughnutLabels = ['Adotados', 'Tutelados', 'Aguardando Adoção', 'Disponíveis'];
+    this.myDoughnutData = [10, 25, 30, 40];
+    this.myListElements =['Adotados', 'Tutelados', 'Aguardando Adoção', 'Disponíveis'];
+    this.calcPercentage(this.myDoughnutData);
   }
 
+  calcPercentage(arr: Array<any>): Array<any>{
+    this.total = arr.reduce((total, numero) => total + numero, 0);
+    this.novo = arr.map(number => (number / 100));
+    return this.myPercentage = this.novo.map(number => (number * this.total));
+  }
 }
