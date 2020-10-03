3e68f35fc97d05188cbb3f45ea5e7b3f5125d71d
Tathi <tathianesouza1@gmail.com>
2020-10-02 23:07:43 -0300

    Correção na chamada ao metodo de validação de erro

--- src/app/_services/validar-inputs.service.ts
+++ src/app/_services/validar-inputs.service.ts
@@ -8,18 +8,18 @@ export class ValidarInputsService {
 
   constructor() { }
 
-  hasErrorValidar(control: AbstractControl, errorName: string): boolean {
+fieldHasError(control: AbstractControl, errorName: string): boolean {
     if ((control.dirty || control.touched) && this.hasError(control, errorName)) { // Se o campo está sujo ou selecionado e possue erro
       return true;
     }
     return false;
   }
 
-  hasError(control: AbstractControl, errorName: string): boolean {
+  hasError(control: AbstractControl, errorName: string): boolean { // Verifica se existe um erro do tipo que queremos validar, ex: required, email
     return control.hasError(errorName);
   }
 
-  lengthValidar(control: AbstractControl, errorName: string): number { // Serve para verificar o tamanho da string passada no formulário, deixando as mensagens dinamicas
+  validateLength(control: AbstractControl, errorName: string): number { // Serve para verificar o tamanho da string passada no formulário, deixando as mensagens dinamicas
     const error = control.errors[errorName];
     return error.requiredLength || error.min || error.max || 0;
   }
