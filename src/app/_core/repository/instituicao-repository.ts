2a6da8dce91cee3679f7ed3b39d30ac909c1a2dd
Tathi <tathianesouza1@gmail.com>
2020-11-02 19:14:49 -0300

    Atualização na listagem de instituições, consumindo dados do back

--- src/app/_core/repository/instituicao-repository.ts
+++ src/app/_core/repository/instituicao-repository.ts
@@ -13,7 +13,7 @@ import { map, mergeMap } from 'rxjs/operators';
 export class InstituicaoRepository {
   mapper = new InstituicaoMapper();
 
-  constructor(public http: BaseHttpService) {}
+  constructor(public http: BaseHttpService) { }
 
   getInstituicaoById(id: number): Observable<InstituicaoModel> {
     return this.http
@@ -21,11 +21,25 @@ export class InstituicaoRepository {
       .pipe(map((x) => this.mapper.mapFrom(x.data)));
   }
 
-  getAllInstituicoes(): Observable<InstituicaoModel> {
-    return this.http
+  async getAllInstituicoes(): Promise<InstituicaoModel[]> {
+    const x = await this.http
       .getAll<InstituicaoEntity[]>(`${environment.URLSERVIDOR}instituicao`)
-      .pipe(mergeMap((x) => x.data))
-      .pipe(map((x) => this.mapper.mapFrom(x)));
+      .toPromise();
+    return x.data.map(this.mapper.mapFrom);
+  }
+
+  async getInstituicoesCidade(id: number): Promise<InstituicaoModel[]> {
+    const x = await this.http
+      .getAll<InstituicaoEntity[]>(`${environment.URLSERVIDOR}instituicao/${id}/cidade`)
+      .toPromise();
+    return x.data.map(this.mapper.mapFrom);
+  }
+
+  async getInstituicoesEstado(id: number): Promise<InstituicaoModel[]> {
+    const x = await this.http
+      .getAll<InstituicaoEntity[]>(`${environment.URLSERVIDOR}instituicao/${id}/estado`)
+      .toPromise();
+    return x.data.map(this.mapper.mapFrom);
   }
 
   postInstituicao(param: InstituicaoModel) {
@@ -45,7 +59,7 @@ export class InstituicaoRepository {
       )
       .pipe(map((x) => x.data));
   }
-  
+
   postImagem(param: any) {
     return this.http.post(`${environment.URLSERVIDOR}imagem`, param);
 
